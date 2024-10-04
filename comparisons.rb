9848df01ed695c2d7896b08ee52e1d1ff966c7a8
Patrick Shim <patrick.shim91@yahoo.com>
2024-10-04 19:49:22 -0400

    added responses to comparisons.rb

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "Is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers == string_teachers", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "is number_students greater than or equal to 21", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -53,24 +60,25 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
-#YOU DO: Explain.
+#YOU DO: the code is evaluating whether 4 is less than 9. It indeed is, so the expression evaluates to true. The puts shows the output to the console so it will print out true. 
 
 books = 3
 puts 4 < books
-# YOU DO: Explain.
+# YOU DO: The number 3 is assigned to a variabled called 'books'. Then the code evaluates whether 4 is less than the the number 3 inside the variable 'books', which  is true. the puts command shows the output to the console and prints out true.
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+# Variable 'friends' is assigned a number 6. The variable called 'sibling' is assigned 2. The code then evaluates to see if the data stored in the variable 'friends' is greater than the data stored in the variable 'siblings', which is true. The puts command displays the out to the console and prints out true.
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+# The variable, 'attendees', is assigned the number 9 while the 'meals' assigned the number 8. Then the expression checks if the data stored in attendees is not equal to the data stored in the 'meals' variable. The puts command displays the result to the console as true.
 
 
-#-------------------
 # PART 3: Logical Operators
 #-------------------
 
@@ -90,16 +98,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats # true
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park # false
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park # true
 
 # Determine if the dog loves to play and is a puppy
 # Assume that a dog is a puppy if it is less than 2 years old.
+puts loves_to_play && age < 2 # true
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: It evaluated to true. This is because the variable 'loves_to_play' is assigned 'true' and the 'age' variable is assigned the number 1, which is less than 2 years old, therefore is a puppy. It is truthy and truthy so the expression evaluates to true.
